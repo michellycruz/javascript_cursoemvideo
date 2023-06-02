17fdb64f63839d4e42144ad2c48d065cb260a763
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-02 12:35:37 -0300

    revisão do while e while

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,20 +1,25 @@
 let contador = 1
 
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 10)
+
+
+/*
+let contador = 1
+
 while (contador <= 10){
     console.log(`Passo ${contador}`)
     contador++
 }
+*/
 
 
 
-
-
-
-
-
-
-
-
-
-
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
 //console.log('Tudo bem?')
