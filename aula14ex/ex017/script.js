40c2d39e7889d396b271b9790bdafd67abcec994
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-03 11:50:49 -0300

    tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -6,6 +6,7 @@ function tabuada(){
     } else{
         let n = Number(num.value)
         let c = 1
+        tab.innerHTML = ''
         while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
