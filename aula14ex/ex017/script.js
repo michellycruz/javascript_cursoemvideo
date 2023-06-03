c4e66e3a381e9666f559f4d2f2760a1c204858e2
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-03 11:46:19 -0300

    tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,4 +1,13 @@
 function tabuada(){
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
+    if(num.value.length == 0){
+        alert("Por favor, digite um número.")
+    } else{
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('option')
+        }
+    }
 }
