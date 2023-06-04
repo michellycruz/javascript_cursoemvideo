741b170be4c1906347197bf3705e6684d0e8bddf
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-04 13:12:40 -0300

    Create funcao01.js

---
+++ aula16/funcao01.js
@@ -0,0 +1,12 @@
+function parimpar(n){
+    if(n%2 == 0){
+        return(`Par!`)
+    } else {
+        return(`Impar!`)
+    }
+}
+
+//let res = parimpar(6)
+//console.log(res)
+
+console.log(parimpar(3))
