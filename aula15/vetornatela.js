0bdf0b54299b51a71c5c9609cf823a701f54c7b5
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-04 11:46:36 -0300

    estrutura for para a exibição dos arrays dentro da variável

---
+++ aula15/vetornatela.js
@@ -0,0 +1,14 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+//console.log(valores)
+
+//console.log(valores[0])
+//console.log(valores[1])
+//console.log(valores[2])
+//console.log(valores[3])
+//console.log(valores[4])
+//console.log(valores[5])
+
+for(let posicao = 0 ; posicao < valores.length ; posicao++){
+    console.log(`A posicao ${posicao} guarda o valor ${valores[posicao]}`)
+}
