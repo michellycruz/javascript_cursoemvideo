cd45a980817fa30b7594adb0f79dc702a58aedd6
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-04 11:21:37 -0300

    Create ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 2, 8, 9, 3]
+
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(num[1])
+console.log(`O primeiro valor do vetor é ${num[0]}`)
