af99325c95f68df4cd3112f1cf66137b718df6ca
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-05 19:13:15 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,18 +1,26 @@
 let number = document.querySelector("input#inum")
-let lista = document.querySelector("input#ilista")
+let lista = document.querySelector("select#ilista")
 let resultado = document.querySelector("div#res")
 let valores = []
 
 
 function isNumber(n){
-    
+    if (Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inList(n){
+
 }
 
 
 function adicionar(){
-    if(isNumber(number.value) && !inList(number.value, valores)){
+    if(isNumber(number.value) && !inList(number.value)){
 
     } else{
-        alert("Resultado inválido ou o número já se encontra na lista.")
+        alert("Resultado inválido ou o número já se encontra na lista")
     }
 }
