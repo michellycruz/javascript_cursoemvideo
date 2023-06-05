864eaf24676022783ec1f2885bc453ec6a94e911
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-05 13:21:59 -0300

    analisador de números

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,16 @@
+let number = document.querySelector("input#inum")
+let lista = document.querySelector("input#ilista")
+let resultado = document.querySelector("div#res")
+let valores = []
+
+
+
+
+
+function adicionar(){
+    if(isNumber(number.value) && !inList(number.value, valores)){
+
+    } else{
+        alert("Resultado inválido ou o número já se encontra na lista.")
+    }
+}
