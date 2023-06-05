298970f9f0cbc22141cb88ff4c341910cca2702a
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-05 20:21:24 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -27,6 +27,7 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert("Resultado inválido ou o número já se encontra na lista")
     }
@@ -39,8 +40,20 @@ function finalizar(){
         alert("Adicione valores antes de finalizar")
     } else {
         let tot = valores.length
+        let maior = valores[0]
+        let menor = valore[0]
+
+        for(let pos in valores){
+            if(valores[pos] > maior)
+            maior = valores[pos]
+
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
 
         res.innerHtml = ''
         res.innerHTML += `<p> Ao todo temos ${tot} números cadastrados.<p>`
+        res.innerHtml = `<p> O maior valor adicionar foi ${maior}.<p>`
+        res.innerHTML =`<p> O menor valor adicionar foi ${menor}.<p>`
     }
 }
